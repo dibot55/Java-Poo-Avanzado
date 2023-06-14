6aa13d6cd6af788d4b94f9514c8336f5d0ee805f
dibot55 <diebots555@gmail.com>
2023-06-14 10:44:54 -0600

    Overload

---
+++ src/Sobrecarga.java
@@ -0,0 +1,13 @@
+public class Sobrecarga {
+    // Sobrecarga de Metodos
+    public int suma(int a, int b){
+        return a+b;
+    }
+    public float suma(float a, float b){
+        return a+b;
+    }
+    public float suma(int a, float b){
+        return a+b;
+    }
+}
+
